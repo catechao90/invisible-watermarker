b94507fa978673bfec464f623222045468affbc4
cate.zhao <zhao.jing@wowoohr.com>
2019-06-03 14:46:48 +0800

    remove watermark-dom, use fix style

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
-var watermark = require("watermark-dom")
-
-export default function(params) {
-  const {markContent, visible = false, opts = {}} = params
-  if (markContent) {
-    setInterval(() => {
-      const clientHeight = Math.max(document.body.scrollHeight, document.body.clientHeight, document.documentElement.clientHeight)
-      const previousHeight = parseInt(sessionStorage.getItem('wartermark-clientHeight'), 0)
-      if (clientHeight !== previousHeight) {
-        sessionStorage.setItem('wartermark-clientHeight', Math.max(document.body.scrollHeight, document.body.clientHeight, document.documentElement.clientHeight))
-        watermark.load({
-        watermark_txt: markContent,
-        watermark_x_space: 0,
-        watermark_y_space: 0,
-        watermark_alpha: visible ?  0.15 : 0.005,
-        ...opts
-        })
-        const parentContainer = document.querySelector('#wm_div_id')
-        const otdiv = document.createElement('div')
-        if (typeof otdiv.createShadowRoot === 'function') {
-          [...parentContainer.shadowRoot.childNodes].forEach(item => {
-            item.style.overflowWrap = 'break-word'
-          })
-        } else {
-          [...parentContainer.childNodes].forEach(item => {
-            item.style.overflowWrap = 'break-word'
-          })
-        }
+export default {
+  isRemove: false,
+  boxid: 'watermark-box',
+  /**
+   * 初始化数字水印
+   * @param {*} watermarkTxt // 水印内容
+   */
+  init (opts = {}) {
+    const {watermarkTxt = '', itemPadding = 0, invisible = true} = opts
+    if (watermarkTxt !== '') {
+      this.isRemove = true
+      let watermarkBox = document.querySelector('#' + this.boxid)
+      if (watermarkBox === null) {
+        watermarkBox = document.createElement('div')
+        watermarkBox.id = this.boxid
+        // 创建水印遮罩层
+        watermarkBox.style.position = 'fixed'
+        watermarkBox.style.zIndex = '99999999'
+        watermarkBox.style.top = '0'
+        watermarkBox.style.left = '0'
+        watermarkBox.style.textAlign = 'center'
+        watermarkBox.style.pointerEvents = 'none'
+        document.body.append(watermarkBox)
       }
-    }, 1000)
+      watermarkBox.innerHTML = ''
+      // 计算要生成水印的数量
+      let watermarkItem = document.createElement('span')
+      watermarkBox.append(watermarkItem)
+      watermarkItem.innerHTML = watermarkTxt
+      let itemStyle = 'display: inline-block;transform: rotate(-30deg); opacity: '+ (invisible? '0.005' : '0.15') +';font-size: 12px; padding: '+ itemPadding +'px'
+      watermarkItem.style = itemStyle
+      let itemHeight = watermarkItem.offsetHeight
+      let itemWidth = watermarkItem.offsetWidth
+      let col = Math.ceil(document.body.offsetWidth / itemWidth)
+      let row = Math.ceil(window.screen.height / itemHeight)
+      let num = col * row
+      watermarkBox.style.width = `${itemWidth * col}px`
+      watermarkItem.remove()
+      // 生产水印
+      for (let i = 0; i < num; i++) {
+        let watermarkItem = document.createElement('span')
+        watermarkItem.innerText = watermarkTxt
+        watermarkItem.style = itemStyle
+        watermarkBox.append(watermarkItem)
+      }
+
+      // 窗口变化时刷新水印
+      window.addEventListener('resize', () => {
+        if (this.isRemove) {
+          this.init(this.watermarkTxt)
+        }
+      })
+    }    
+  },
+  /**
+   * 更新水印内容
+   * @param {*} watermarkTxt // 水印内容
+   */
+  update (watermarkTxt) {
+    this.init(watermarkTxt)
+  },
+  /**
+   * 移除水印
+   */
+  remove () {
+    let watermarkBox = document.querySelector('#' + this.boxid)
+    if (watermarkBox !== null) {
+      watermarkBox.remove()
+    }
   }
 }
